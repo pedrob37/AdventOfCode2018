e743ac75bfdd5067170fd680bbc664ea0e0a56f3
Pedro Borges <p.borges.17@ucl.ac.uk>
2018-12-06 22:19:15 +0000

    Finally completed day4: Not at all pretty but works

--- Day4/day4.py
+++ Day4/day4.py
@@ -4,53 +4,102 @@ import numpy as np
 import os
 import re
 from datetime import datetime
+from scipy import stats
 
 os.chdir(r'C:\Users\pedro\Desktop\AdventOfCode\Day4')
 lines = [line.rstrip('\n') for line in open('input.txt')]
-
-# Define date format
-date_format = "%Y-%m-%d %H:%M"
-time1 = datetime.strptime(sorted_dates[0][0], date_format)
 
 # Let's split into: date, time, info
 date_list = []
 for row in range(len(lines)):
     date_list.append(re.split('\[|] ', lines[row])[1:])
 sorted_dates = sorted(date_list, key=lambda x: x[0])
-# sorted(lines, key=lambda d: tuple(map(int, d[0].split('-'))))
 
-# Time differences
-d0 = date(*tuple(map(np.int, (re.split('-', sorted_dates[0][0][0:11])))))
-d1 = date(*tuple(map(np.int, (re.split('-', sorted_dates[1000][0][0:11])))))
-delta = d1 - d0
-print(delta.days)
-
-day_first = datetime.strptime(sorted_dates[0][0], date_format)
-day_last = datetime.strptime(sorted_dates[-1][0], date_format)
-num_days = day_last - day_first
-
-
-class Guard:
-    def __init__(self, info):
-        date_format = "%Y-%m-%d %H:%M"
-        self.date = datetime.strptime(info[0], date_format)
-        self.text = re.split(' ', info[1])[0]
-
-    def find_type(self):
-        if 'Guard' in self.text:
-            ID = re.split('#| |', string)[2]
-            return ID
+# %%
+date_format = "%Y-%m-%d %H:%M"
+tot_days = (datetime.strptime(sorted_dates[-1][0], date_format) -
+            datetime.strptime(sorted_dates[0][0], date_format)).days + 1
+timings = np.zeros([tot_days, 62], dtype=np.uint16)  # Date then guard #
+day_ind = -1
+for index, entry in enumerate(sorted_dates):
+    date = datetime.strptime(entry[0], date_format)
+    text = re.split(' ', entry[1])
+    if 'Guard' in text:
+        if index != 0:
+            timings[day_ind, prev_tracker+2:] = 0
+        day_ind += 1
+        if date.hour == 23:
+            day = date.day + 1
         else:
-            month = self.date.month
-            day = self.date.day
-            time = self.date.minute
-            state = self.text
-            return month, day, time
+            day = date.day
+        timings[day_ind, 0] = day
+        ID = text[1][1:]
+        timings[day_ind, 1] = ID
+        prev_tracker = 0
+    elif 'falls' in text:
+        minute = date.minute
+        timings[day_ind, prev_tracker+2:minute+2] = 0
+        prev_tracker = minute
+    elif 'wakes' in text:
+        minute = date.minute
+        timings[day_ind, prev_tracker+2:minute+2] = 1
+        prev_tracker = minute
 
-    def 
+# %%
+# Need to find minute MODE NOT TOTAL
+guard_ids = list(np.unique(timings[:, 1]))
+guard_tots = np.zeros([len(guard_ids)])
+for day in range(tot_days):
+    current_id = timings[day, 1]
+    guard_tots[guard_ids.index(current_id)] += np.sum(timings[day, 2:])
+guard_tots = list(guard_tots)
+bad_guard = guard_ids[guard_tots.index(max(guard_tots))]
 
-info_box = []
-for row in range(len(sorted_dates)):
-    if sorted_dates[row][1][0] == 'G':
-        
-        info_box.append(sorted_dates[row][0][5:10] + )
+minutes = []
+for day in range(tot_days):
+    current_id = timings[day, 1]
+    if current_id == bad_guard:
+        for minute in range(60):
+            if timings[day, minute+2] == 1:
+                minutes.append(minute)
+minutes_mode, _ = stats.mode(minutes)
+print(int(minutes_mode)*bad_guard)
+
+# %% Part Two: Find guard that falls asleep on the same minute most often
+date_format = "%Y-%m-%d %H:%M"
+tot_days = (datetime.strptime(sorted_dates[-1][0], date_format) -
+            datetime.strptime(sorted_dates[0][0], date_format)).days + 1
+timings = np.zeros([tot_days, 62], dtype=np.uint16)  # Date then guard #
+day_ind = -1
+for index, entry in enumerate(sorted_dates):
+    date = datetime.strptime(entry[0], date_format)
+    text = re.split(' ', entry[1])
+    if 'Guard' in text:
+        if index != 0:
+            timings[day_ind, prev_tracker+2:] = 1e5*np.random.rand()
+        day_ind += 1
+        if date.hour == 23:
+            day = date.day + 1
+        else:
+            day = date.day
+        timings[day_ind, 0] = day
+        ID = text[1][1:]
+        timings[day_ind, 1] = ID
+        prev_tracker = 0
+    elif 'falls' in text:
+        minute = date.minute
+        timings[day_ind, prev_tracker+2:minute+2] = 1e5*np.random.rand()
+        prev_tracker = minute
+    elif 'wakes' in text:
+        minute = date.minute
+        timings[day_ind, prev_tracker+2:minute+2] = timings[day_ind, 1]
+        prev_tracker = minute
+
+minute_modes = np.zeros([60, 2])
+best = np.array([0, 0, 0])
+for minute in range(60):
+    minute_mode, frequency = stats.mode(timings[:, minute+2])
+    minute_modes[minute, :] = [int(minute_mode), int(frequency)]
+    if int(frequency) > best[2]:
+        best = [minute, int(minute_mode), int(frequency)]
+print(best[0]*best[1])
